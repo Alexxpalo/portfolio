612b48798ffc451f1ac3d5be2fc859b86621a9e8
Alexxpalo <palosaaritobias@gmail.com>
2023-01-18 13:42:48 +0200

    update

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
+
 function ProjectCard(props) {
-  return (<div className="bg-slate-200 text-xl w-1/3 px-1 flex flex-col gap-1">
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div onClick={() => setIsOpen(!isOpen)} className="bg-slate-200 text-xl w-1/3 px-1 flex flex-col gap-1">
     <p className="text-center bg-white p-2 text-blue-500">{props.name}</p>
     <img src={"img/" + props.image + ".webp"} alt={props.name} />
+
+    {isOpen && (
+      <p>open</p>
+    )}
     </div>
-  
   );
 }
 
